f47783a1d97d81f0dacb22b49f911b6fa1f588c6
John Foley <jfoley@cs.umass.edu>
2018-11-21 17:15:22 -0500

    move toybox core to its own repo

---
+++ src/direction.rs
@@ -0,0 +1,32 @@
+use super::Input;
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Serialize, Deserialize)]
+pub enum Direction {
+    Up,
+    Down,
+    Left,
+    Right,
+}
+impl Direction {
+    pub fn delta(self) -> (i32, i32) {
+        match self {
+            Direction::Up => (0, -1),
+            Direction::Down => (0, 1),
+            Direction::Left => (-1, 0),
+            Direction::Right => (1, 0),
+        }
+    }
+    pub fn from_input(buttons: Input) -> Option<Direction> {
+        if buttons.up {
+            Some(Direction::Up)
+        } else if buttons.down {
+            Some(Direction::Down)
+        } else if buttons.left {
+            Some(Direction::Left)
+        } else if buttons.right {
+            Some(Direction::Right)
+        } else {
+            None
+        }
+    }
+}
